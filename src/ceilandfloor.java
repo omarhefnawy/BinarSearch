bea2d25bc66a0f87b639441e87963bcef5bce3ac
OmarHefnawy Mohamed <omarhefhef90@gmail.com>
2024-07-03 05:54:39 +0300

    rotated msolution mean 3456 7 012

--- src/ceilandfloor.java
+++ src/ceilandfloor.java
@@ -4,7 +4,7 @@ public class ceilandfloor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  int [] list = {-1000,-3,-1,0,4,30,55,90,110,200,300,500,1000,1500};
- int CeilAnswer= Ceiling(list,2000);
+ int CeilAnswer= Ceiling(list,1100);
  int FloorAnswer= Flooring(list,80);
  if (CeilAnswer>list.length-1)
  {
